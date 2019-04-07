d2a70f646008d0cad3235a27f9517b1a02c60de1
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-04-07 06:02:15 +0000

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,1 +1,1 @@
-bat 'copy C:/Program Files (x86)/Jenkins/jobs/os-sample-java-web/branches/master/workspace/target/*.war C:/Program Files/Apache-Software-Foundation/Tomcat-8.5/webapps'
+mvn tomcat7::run
