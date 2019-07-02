f7d8da4772caf227df7ac993d979ea2983210e41
Ajayvarma8142 <46514718+Ajayvarma8142@users.noreply.github.com>
2019-07-02 18:23:52 +0530

    Update runapp.bat

--- runapp.bat
+++ runapp.bat
@@ -1,2 +1,4 @@
 mvn tomcat8:deploy
 copy c:\progra~2\Jenkins\jobs\Ajay\branches\master\workspace\target\*.war C:\progra~1\Apache-Software-Foundation\Tomcat-8.5\webapps
+
+## you can also use "  " to reduce spaces ex: xcopy /e dist\* "D:\Apache-Software-Foundation\Tomcat 8.5-1\webapps"
